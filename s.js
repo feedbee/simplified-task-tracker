2ab426fea339b0df7ceeb1fe6479f797bf58374b
Valera Leontyev <feedbee@gmail.com>
2013-10-03 10:02:34 +0300

    +Drag-n-Drop, small refactor

--- s.js
+++ s.js
@@ -1,25 +1,54 @@
-var data = [
-	{test_server: null, status:"dev", no: 1, title: "Task 1"},
-	{test_server: "test", status:"test", no: 2, title: "Task 2"},
-	{test_server: "new", status:"test", no: 3, title: "Task 3"},
-	{test_server: null, status:"dev", no: 4, title: "Task 4"},
-	{test_server: "new", status:"done", no: 5, title: "Task 5"},
-	{test_server: null, status:"dev", no: 6, title: "Task 6"}
-];
+var Stt = function () {
 
-function draw(data) {
-	for (var x in data) {
-		var b = $('#c-tpl').clone().attr('id', null);
-		var h = $('<div>').append(b).html();
-		h = setVars(data[x], h);
-		$('#c').append(h);
+	// public 
+
+	this.init = function () {
+		draw();
+		$("#c").sortable({
+			update: function(event, ui) {
+				console.log(ui);
+			}
+		});
+		$("#c").disableSelection();
 	}
 	
-}
+	// private
 
-function setVars(vars, text) {
-	for (var v in vars) {
-		text = text.replace(new RegExp('\\{\\{' + v + '\\}\\}'), vars[v]);
+	var data = [
+		{test_server: null, status:"dev", no: 1, title: "Task 1"},
+		{test_server: "test", status:"test", no: 2, title: "Task 2"},
+		{test_server: "new", status:"test", no: 3, title: "Task 3"},
+		{test_server: null, status:"dev", no: 4, title: "Task 4"},
+		{test_server: "new", status:"done", no: 5, title: "Task 5"},
+		{test_server: null, status:"dev", no: 6, title: "Task 6"}
+	];
+
+	var sort = function () {
+		data.sort(cmp);
 	}
-	return text;
-}
+	var cmp = function (a, b) {
+		return a.no > b.no ? 1 : (a.no < b.no ? -1 : 0);
+	}
+
+	var draw = function () {
+		for (var x in data) {
+			var b = $('#c-tpl').clone().attr('id', null);
+			var h = $('<div>').append(b).html();
+			h = setVars(data[x], h);
+			$('#c').append(h);
+		}
+	}
+
+	var setVars = function (vars, text) {
+		for (var v in vars) {
+			text = text.replace(new RegExp('\\{\\{' + v + '\\}\\}'), vars[v]);
+		}
+		return text;
+	}
+};
+
+Stt.init = function () {
+	Stt.instance = new Stt();
+	Stt.instance.init();
+	return Stt.instance;
+};
